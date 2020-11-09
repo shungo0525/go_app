a326d52a68dfb619f8d885b39561d35b45249ae7
shungo0525 <shungo0525@gmail.com>
2020-11-09 09:35:35 +0900

    インデント修正

--- src/main.go
+++ src/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    router := gin.Default()
-    router.LoadHTMLGlob("templates/*.html")
+	data := "Hello Go/Gin!!"
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
 
-    router.GET("/", func(ctx *gin.Context){
-        ctx.HTML(200, "index.html", gin.H{})
-    })
-    router.GET("/show", func(ctx *gin.Context){
-        ctx.HTML(200, "show.html", gin.H{})
-    })
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{"data": data})
+	})
+	router.GET("/show", func(ctx *gin.Context) {
+		ctx.HTML(200, "show.html", gin.H{})
+	})
 
-    router.Run()
+	router.Run(":3000")
 }
